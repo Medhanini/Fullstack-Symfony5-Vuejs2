72d3e9ffec392babccc1440b7b3e6d28f7d0619b
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-11-13 08:17:24 +0100

    requiring  [ Form & validator & annotations ]  => done

---
+++ symfony-server/src/Form/CustomerType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('PhoneNumber')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
